9695a7fb5e770f2200e83f59ffcb1919613cea68
Haowei Hsu <hwhsu1231@gmail.com>
2025-06-01 01:14:05 +0800

    🐛 fix(ltd-flyout): remove box-shadow from select dropdown
    - remove box-shadow to align with design guidelines

--- ltd-flyout.js
+++ ltd-flyout.js
@@ -305,6 +305,7 @@ function addStyles() {
       line-height: inherit;
       width: 100%;
       box-sizing: border-box;
+      box-shadow: none;
       background-image: url('data:image/svg+xml;utf8,<svg fill="%23ffffff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'); /* Custom arrow */
       background-repeat: no-repeat;
       background-position: right 5px center;
